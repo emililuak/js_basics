ba5002e860a3fd2cb10cc56b7ac1a355f55e0b73
Emili <emili.kaul@voco.ee>
2022-09-14 10:57:34 +0300

    text output and commets

---
+++ variables&datatypes/app.js
@@ -0,0 +1,20 @@
+/**
+ * documanetaion
+ * comment
+ * style
+ */
+
+// this is simple comment
+/*
+console.log("second row")
+console.log("second row")
+console.log("third row") */
+
+console.log("second row")
+console.log("second row")
+console.log("third row")
+console.log("third row")
+
+
+
+
