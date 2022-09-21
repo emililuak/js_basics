d18baf08373c75f900fd57cba8a2c3c55e07f046
Emili <emili.kaul@voco.ee>
2022-09-21 10:50:46 +0300

    astmed

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -1,20 +1,6 @@
-/**
- * datatypes
- *
- * primitives - integer, float
- * objects
- *
- * variables = var, let, const
- *
- * let variableName = value // mutuable
- * const constName = value // immutable
- */
+let astmealus = prompt("Sisestage astme alus: ")
+let astendaja = prompt("Sisestage astendaja: ")
 
-let aasta = 2016
-let president = "Kersti Kaljulaid"
-let lause_keskosa = ". aastal valiti Eesti presidendiks"
-let lause
+let tulemus = Math.pow(astmealus, astendaja)
 
-
-
-
+console.log(tulemus)
