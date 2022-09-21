56807a2ad1fdba1c440ff3f886fbe04270515f2a
Emili <emili.kaul@voco.ee>
2022-09-21 11:12:45 +0300

    katkine "lähme sõidame"

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -1,8 +1,9 @@
-let astmealus = Number(prompt("Sisestage astme alus: "))
-let astendaja = Number(prompt("Sisestage astendaja: "))
+let nimi =prompt("Sisestage oma nimi: ")
+let lubatudKiirus = Number(prompt("Sisestage lubatud kiirus: "))
+let tegelikKiirus = Number(prompt("Sisetage tegelik kiirus: "))
 
-console.log(astmealus, typeof astmealus)
-console.log(typeof  astendaja)
+let trahv = (tegelikKiirus - lubatudKiirus) * 3
+let tegeliktrahv = Math.min(trahv, 200)
 
-let tulemus = Math.pow(astmealus, astendaja)
-console.log(tulemus)
+
+console.log(nimi + ", kiiruse ületamise eest on teie trahv " + tegeliktrahv + " eurot")
