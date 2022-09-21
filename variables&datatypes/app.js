f8c4c3fbde76e78b47c5025e2f999e438ef52d11
Emili <emili.kaul@voco.ee>
2022-09-21 10:59:19 +0300

    astmed

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -1,6 +1,8 @@
-let astmealus = prompt("Sisestage astme alus: ")
-let astendaja = prompt("Sisestage astendaja: ")
+let astmealus = Number(prompt("Sisestage astme alus: "))
+let astendaja = Number(prompt("Sisestage astendaja: "))
+
+console.log(astmealus, typeof astmealus)
+console.log(typeof  astendaja)
 
 let tulemus = Math.pow(astmealus, astendaja)
-
 console.log(tulemus)
