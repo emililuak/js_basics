7441834419f18b7d3546db525345f50dc9542285
Emili <emili.kaul@voco.ee>
2022-09-29 12:51:38 +0300

    while 3.2

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -1,9 +1,13 @@
-let kordamise_arv = Number(prompt("Sisestage, mitu x äratada: "))
-
-let kord = 1 // tsükli juhtimismuutuja algväärtus
-//korduvad tegevused
-while(kord <= kordamise_arv){
-    console.log(`Tõuse ja sära - ${kord}. kord!`)
-    kord++
+let ringide_arv = Number(prompt("Sisesta ringide arv: "))
+let porgandid_kokku = 0
+let ring = 1
+while (ring <= ringide_arv){
+    //console.log(`${ring}. ring`)
+    if(ring % 2 == 0){
+        //console.log(`Saab ${ring} porgandit`)
+        porgandid_kokku = porgandid_kokku + ring // porgangid_kokku += ring
+        //console.log(porgandid_kokku)
+    }
+    ring++
 }
-console.log("Programmi lõpp")
+console.log(`Porgandite koguarv on ${porgandid_kokku}.`)
