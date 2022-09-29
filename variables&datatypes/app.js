65f48eef0ba44e965793a39621258f70c8aa27bd
Emili <emili.kaul@voco.ee>
2022-09-29 13:38:53 +0300

    for 3.3

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -1,13 +1,6 @@
-let ringide_arv = Number(prompt("Sisesta ringide arv: "))
-let porgandid_kokku = 0
-let ring = 1
-while (ring <= ringide_arv){
-    //console.log(`${ring}. ring`)
-    if(ring % 2 == 0){
-        //console.log(`Saab ${ring} porgandit`)
-        porgandid_kokku = porgandid_kokku + ring // porgangid_kokku += ring
-        //console.log(porgandid_kokku)
-    }
-    ring++
+let täring_arv = Number(prompt("sisesta vajalik täringute arv: "))
+
+for (let kord = 1; kord <= täring_arv; kord++){
+    let täring = Math.ceil(Math.random() * 6)
+    console.log(täring)
 }
-console.log(`Porgandite koguarv on ${porgandid_kokku}.`)
